6e2f662a4e8728722159c867f206bc1e29b58ed2
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-07-07 20:22:13 -0400

    added the global and const variables to the js

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,32 @@
-// Assignment code here
+// global variables 
+const minPwdLength = 8;
+const maxPwdLength = 128;
+
+
+var lowerInfo = {
+    theChars: "abcdefghijklmnopqrstuvwxyz",
+    required: false,
+    name: "lowercase"
+}
+
+var upperInfo = {
+    theChars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    required: false,
+    name: "uppercase" 
+}
+
+var numericInfo = {
+    theChars: "0123456789",
+    required: false,
+    name: "numeric"
+}
+
+var specialInfo = {
+    theChars: " !#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+    required: false,
+    name: "special"
+}
+
 
 
 // Get references to the #generate element
